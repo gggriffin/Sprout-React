63675be38d5cf9b7b59b75575950dc8ed9b02663
Jason Moore <jason.moore1369@gmail.com>
2019-07-24 21:10:23 -0500

    Projects.css filled. minor correction to Collaborate.css

--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -23,32 +23,32 @@ function Project() {
         </Col>
       </Row>
       <Row>
-        <Col md={{ span: 12 }}>
-          <Card>
+        <Col md={{ span: 10, offset: 1 }}>
+          <Card id="projCard">
             <Row>
-              <Col md={{ span: 8, offset: 2 }}>
-                <Card.Title>
+              <Col>
+                <Card.Title id="projCardTitle">
                   <h1>Bamazon</h1>
                 </Card.Title>
               </Col>
             </Row>
             <Row>
-              <Col md={{ span: 4 }}>
+              <Col md={{ span: 4 }} id="projIcon">
                 <i class="devicon-nodejs-plain"></i>
                 <i class="devicon-sequelize-plain"></i>
                 <i class="devicon-mysql-plain"></i>
                 <i class="devicon-jquery-plain-wordmark"></i>
               </Col>
-              <Col md={{ span: 8 }}>
+              <Col md={{ span: 8 }} id="projDesc">
                 <p>
                   This is an in-line command app that uses Sequelize to interface between Node.js and MySQL. jQuery is used to help increase the readability of the Javascript code. The application allows the user to directly interface with MySQL from a Node.js server and buy items from a virtual store front. The information is stored in a MySQL database, which allows the data to remain persistent.
                 </p>
-                <h3>Intermediate</h3>
-                <i class="devicon-github-plain">
-                  <a href="https://github.com/gggriffin/bamazon"></a>
-                </i>
-                <i class="fas fa-seedling">
+                <h3 id="intermed">Intermediate</h3>
+                <i class="fas fa-seedling" id="seedIcon">
                   <a href="https://drive.google.com/file/d/1ewEkheW4vO0sjkkJ2ax3VeNAKEK5Ng0Y/view"></a>
+                </i>
+                <i class="devicon-github-plain" id="gitIcon">
+                  <a href="https://github.com/gggriffin/bamazon"></a>
                 </i>
               </Col>
             </Row>
@@ -56,32 +56,32 @@ function Project() {
         </Col>
       </Row>
       <Row>
-        <Col md={{ span: 12 }}>
-          <Card>
+        <Col md={{ span: 10, offset: 1 }}>
+          <Card id="projCard">
             <Row>
-              <Col md={{ span: 8, offset: 2 }}>
-                <Card.Title>
+              <Col>
+                <Card.Title id="projCardTitle">
                   <h1>Train Finder</h1>
                 </Card.Title>
               </Col>
             </Row>
             <Row>
-              <Col md={{ span: 4 }}>
+              <Col md={{ span: 4 }} id="projIcon">
                 <i class="devicon-html5-plain-wordmark"></i>
                 <i class="devicon-css3-plain-wordmark"></i>
                 <i class="devicon-jquery-plain-wordmark"></i>
                 <i class="devicon-bootstrap-plain-wordmark"></i>
               </Col>
-              <Col md={{ span: 8 }}>
+              <Col md={{ span: 8 }} id="projDesc">
                 <p>
                   This is an in-line command app that uses Sequelize to interface between Node.js and MySQL. jQuery is used to help inrease the readability of the Javascript code.
                 </p>
-                <h3>Begginer</h3>
-                <i class="devicon-github-plain">
-                  <a href="https://github.com/gggriffin/bamazon"></a>
-                </i>
-                <i class="fas fa-seedling">
+                <h3 id="begginer">Begginer</h3>
+                <i class="fas fa-seedling" id="seedIcon">
                   <a href="https://drive.google.com/file/d/1ewEkheW4vO0sjkkJ2ax3VeNAKEK5Ng0Y/view"></a>
+                </i>
+                <i class="devicon-github-plain" id="gitIcon">
+                  <a href="https://github.com/gggriffin/bamazon"></a>
                 </i>
               </Col>
             </Row>
